0ad1b666b18648c54b02618c3753ffcb0464dc49
Emil Berbakov <emberbakov@gmail.com>
2023-05-26 00:19:40 -0500

    Add project files.

---
+++ TheBerbakery.API/Controllers/RecipeCardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheBerbakery.API.Controllers
+{
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [Route("RecipeCard.API/[controller]")]
+    [ApiController]
+    public class RecipeCardController : ControllerBase
+    {
+        #region Service Constructor
+
+        #endregion
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("recipeCards")]
+
+    }
+
+}
